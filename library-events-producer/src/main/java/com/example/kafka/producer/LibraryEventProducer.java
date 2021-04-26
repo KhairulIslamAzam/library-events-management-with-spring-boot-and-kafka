60a2f32b317b325dffae13b45d8a90e10153b6f8
KhairulIslamAzam <khairulisla.azam@gmail.com>
2021-04-26 09:33:30 +0600

    added kafka producer for sending message

---
+++ library-events-producer/src/main/java/com/example/kafka/producer/LibraryEventProducer.java
@@ -0,0 +1,59 @@
+package com.example.kafka.producer;
+
+import com.example.kafka.entity.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+/**
+ * @author Khairul Islam Azam
+ * @created 26/04/2021 - 9:19 AM
+ * @project IntelliJ IDEA
+ */
+@Component
+@Slf4j
+public class LibraryEventProducer {
+    @Autowired
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
+        listenableFuture.addCallback(new ListenableFutureCallback<>() {  //same as new ListenableFutureCallback<SendResult<Integer, String>>()
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending the message and the exception is {}", ex.getMessage());
+        try {
+            throw ex;
+        }catch (Throwable throwable){
+            log.error("Error in onFailure: {}", throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message sent Successfully for the key : {} and the value is {} , partitions is {}",
+                key,value,result.getProducerRecord().partition());
+    }
+}
